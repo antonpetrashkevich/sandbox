79d1b5e201ed205d86f5f0ca8c750713a7e5fd94
Anton Petrashkevich <anton.petrashkevich@gmail.com>
2013-01-02 14:12:43 +0300

    HelloWorld Ant project

---
+++ Ant/src/com/mypackage/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.mypackage;
+
+/**
+ * User: Anton
+ * Date: 02.01.13
+ * Time: 4:38
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

0c65192143d7a271c72712ae0140a884cb3f8b6b
Anton Petrashkevich <anton.petrashkevich@gmail.com>
2013-05-14 17:53:57 +0300

    Spring. AOP. Required annotation added

--- spring/src/main/java/foo/bar/aop/model/Circle.java
+++ spring/src/main/java/foo/bar/aop/model/Circle.java
@@ -1,11 +1,14 @@
 package foo.bar.aop.model;
 
+import org.springframework.beans.factory.annotation.Required;
+
 /**
  * User: Anton
  * Date: 14.05.13
  * Time: 12:20
  */
 public class Circle extends Shape {
+
 
     private String name;
 
@@ -14,6 +17,7 @@ public class Circle extends Shape {
         return name;
     }
 
+    @Required
     public void setName(String name) {
         this.name = name;
     }
